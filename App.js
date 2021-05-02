995146706b85b8ebcffd96ce2060144f0a47a52a
shakilmahammud <hshakil987@gmail>
2021-05-02 23:52:41 +0600

    update

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const [entereGoal,setEnterGoal]=useState('')
+
+  const addGoalHandler =()=>{
+   console.log(entereGoal)
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Hello Our First App</Text>
+      <Button title="Add" onPress={addGoalHandler}>Add</Button>
     </View>
   );
 }
