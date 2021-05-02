17b83503ce6a30533fbd117a98225a82f3b7399a
shakilmahammud <hshakil987@gmail>
2021-05-03 00:23:51 +0600

    apps update

---
+++ components/InputPlace/InputPlace.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
+
+const InputPlace = props => {
+    return (
+        <View style={styles.inputView}>
+            <TextInput
+                style={{
+                    width: "80%",
+                    borderBottomWidth: 1,
+                    borderColor: "green",
+                    padding: 7
+                }}
+                placeholder="Add a Place.."
+                value={props.inputValue}
+                onChangeText={text => props.setInputValue(text)}
+            />
+            <Button
+                title="Add"
+                onPress={() => {
+                    if (props.inputValue !== "") {
+                        props.setPlaceList([...props.placeList, {
+                            key: Math.random().toString(),
+                            value: props.inputValue,
+                            image: {
+                                uri: "https://cdn.britannica.com/97/189797-050-1FC0041B/Night-view-Dhaka-Bangladesh.jpg"
+                            }
+                        }]);
+                        props.setInputValue("");
+                    }
+                }}
+            />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    inputView: {
+        padding: 20,
+        width: "100%",
+        marginTop: 50,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center'
+    }
+});
+
+export default InputPlace;
